3852752393219a9574f15bc67c3e277539cc4379
JusticioCaesario <justicio.caesario@mhs.unsoed.ac.id>
2023-07-12 13:48:02 +0700

    dashboard admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pengunjung;
 
 class DashboardController extends Controller
 {
-    public function index(){
-       return view ('dashboard');
+    public function index()
+    {
+        // Ambil data pengunjung dari tabel tbl_pengunjung
+        $pengunjung = Pengunjung::all();
+
+        // Tampilkan halaman dashboard dengan data pengunjung
+        return view('dashboard', compact('pengunjung'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,55 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Dashboard</title>
-    <!-- Tambahkan CSS atau script tambahan sesuai kebutuhan -->
+    <title>Dashboard Admin</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/dashboard.css') }}">
 </head>
 <body>
-    <h1>Welcome to Dashboard</h1>
-    <!-- Tambahkan konten halaman dashboard sesuai kebutuhan -->
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <h2>Admin Dashboard</h2>
+        <ul>
+            <li><a href="#form">Form</a></li>
+            <li><a href="#data">Data Pengunjung</a></li>
+            <!-- Tambahkan menu navigasi lainnya sesuai kebutuhan -->
+        </ul>
+    </div>
+
+    <!-- Content -->
+    <div class="content">
+        <h1>Dashboard Admin</h1>
+
+        <!-- Tabel Data Pengunjung -->
+        <section id="data">
+            <h2>Data Pengunjung</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nama</th>
+                        <th>Tanggal</th>
+                        <th>Jenis Kelamin</th>
+                        <th>Alamat</th>
+                        <th>No. HP</th>
+                        <th>Maksud</th>
+                        <th>Saran</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Loop untuk menampilkan data pengunjung dari tbl_pengunjung -->
+                    @foreach ($pengunjung as $data)
+                    <tr>
+                        <td>{{ $data->nama }}</td>
+                        <td>{{ $data->tanggal }}</td>
+                        <td>{{ $data->jenkel }}</td>
+                        <td>{{ $data->alamat }}</td>
+                        <td>{{ $data->nohp }}</td>
+                        <td>{{ $data->maksud }}</td>
+                        <td>{{ $data->saran }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </section>
+    </div>
 </body>
 </html>
