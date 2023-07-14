28bab22bc63d23e3079cdb106e94ae94a406cc1c
JusticioCaesario <justicio.caesario@mhs.unsoed.ac.id>
2023-07-14 11:36:49 +0700

    migrate

--- database/migrations/2023_07_14_042917_add_opd_id_to_tbl_pengunjung.php
+++ database/migrations/2023_07_14_042917_add_opd_id_to_tbl_pengunjung.php
@@ -1,21 +1,22 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddOpdIdToUsers extends Migration
+class AddOpdIdToTblPengunjung extends Migration
 {
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->unsignedInteger('id_opd')->nullable();
+        Schema::table('tbl_pengunjung', function (Blueprint $table) {
+            $table->unsignedInteger('id_opd')->after('saran')->nullable();
             $table->foreign('id_opd')->references('id')->on('opd');
         });
     }
 
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('tbl_pengunjung', function (Blueprint $table) {
             $table->dropForeign(['id_opd']);
             $table->dropColumn('id_opd');
         });
